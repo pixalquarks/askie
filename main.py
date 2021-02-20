d726c9c6759e809d52e84f4392fd4d3d928282da
Rakshat Kashyap <59429789+pixalquarks@users.noreply.github.com>
2021-02-21 00:48:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -94,8 +94,8 @@ async def showWaifu(ctx):
 
 @client.command()
 async def introduce(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+# 	if not isChannelNormal(ctx):
+# 		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	message = f"""konnichiwa {ctx.message.author} aniki, Boku wa GimmeALove booto. I'm currently under development,
 			    so forgive me if anything goes wrong.
 				Gimme a pat hehe, that will cheer me a lot.
@@ -112,8 +112,8 @@ async def introduce(ctx):
 
 @client.command(aliases=["ELP",'Elp'])
 async def elp(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You cannot use this command from this channel")
+# 	if not isChannelNormal(ctx):
+# 		return await ctx.send("You cannot use this command from this channel")
 	message = f"""Yo, {ctx.message.author} aniki, Currently I support these feautes:
 				1. Waifu  Commands availabe:
 					a. $random/Random used to spawn a random waifu
@@ -183,8 +183,8 @@ async def displayWaifu(ctx,data:dict):
 
 @client.command(aliases=["waifustatus","Waifustatus"])
 async def find_waifu_by_status(ctx,status:str):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	data = None
 	if status in ("legendary","superrare","common","special"):
 		data = db.waifu_status(status)
@@ -195,8 +195,8 @@ async def find_waifu_by_status(ctx,status:str):
 
 @client.command(aliases=["Waifuname","waifuname"])
 async def get_waifu_by_name(ctx,name:str):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	data = db.find_waifu(name)
 	ln = list(data)
 	lm = [i["waifu"]["name"] for i in ln]
@@ -255,40 +255,40 @@ async def manageEcchi(ctx,what:str):
 
 @client.command(aliases=["Thick","thick"])
 async def getThick(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	await manageEcchi(ctx,"thick")
 
 @client.command(aliases=["imouto","Imouto"])
 async def getImouto(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	await manageEcchi(ctx,"imouto")
 
 @client.command(aliases=["milf","Milf"])
 async def getMilf(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	await manageEcchi(ctx,"milf")
 
 @client.command()
 async def nsfw(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	await manageEcchi(ctx,"nsfw")
 
 
 
 @client.command(aliases=["PING","Ping"])
 async def ping(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 	await ctx.send(f"PONG! {round(client.latency * 1000)} ms")
 
 @client.command(aliases=["Random"])
 async def random(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 
 	global dictn
 	global random_search_done
@@ -307,8 +307,8 @@ async def random(ctx):
 
 @client.command()
 async def catch(ctx, *, name):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You can only use this commnad from gimme_a_waifu channel. Don't spam this here to avoid a ban")
 		
 	if name == None:
 		await ctx.send("Enter a valid name")
@@ -325,8 +325,8 @@ async def catch(ctx, *, name):
 
 @client.command(aliases=["toggle"])
 async def toggle_doujin(ctx):
-	if not isChannelDoujin(ctx):
-		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
+#	if not isChannelDoujin(ctx):
+#		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
 	if not check_authority(ctx):
 		return await ("You are not eligible for this command. Only mods can config this")
 	global disable_doujin
@@ -337,8 +337,8 @@ async def toggle_doujin(ctx):
 
 @client.command(aliases=["find"])
 async def getDoujin(ctx, id:int):
-	if not isChannelDoujin(ctx):
-		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
+#	if not isChannelDoujin(ctx):
+#		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
 	global disable_doujin
 	if disable_doujin:
 		return await ctx.send("Seems like mods have disable doujin on this channel. Try asking them for the permissions")
@@ -362,8 +362,8 @@ async def getDoujin(ctx, id:int):
 
 @client.command()
 async def read(ctx):
-	if not isChannelDoujin(ctx):
-		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
+#	if not isChannelDoujin(ctx):
+#		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
 	global read_
 	global doujin
 	if not doujin:
@@ -374,8 +374,8 @@ async def read(ctx):
 
 @client.command(aliases=["/"])
 async def doujin(ctx, cmd:str, page=0):
-	if not isChannelDoujin(ctx):
-		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
+#	if not isChannelDoujin(ctx):
+#		return await ctx.send("You cannot use this command from this channel. Only from Doujin channel")
 	global read_
 	if not read_:
 		return await ctx.send("Please enter the readmode first. Use the command $read to enter readmode")
@@ -412,41 +412,41 @@ async def doujin(ctx, cmd:str, page=0):
 		doujin = {}
 		return await ctx.send("Deleted the previous doujin. Use command $getDoujin <id> to find a new one")
 
-@client.command(aliases=["askie","aski-chan"])
-async def call_aski_chan(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You cannot use this command from this channel")
-	filename = os.path.join(os.getcwd() + "/Musics",choice(configs.Normal_onii_chan_sounds))
-	file = discord.File(filename,filename="onii_chan.mp3")
-	await ctx.send(file=file)
+#@client.command(aliases=["askie","aski-chan"])
+#async def call_aski_chan(ctx):
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You cannot use this command from this channel")
+#	filename = os.path.join(os.getcwd() + "/Musics",choice(configs.Normal_onii_chan_sounds))
+#	file = discord.File(filename,filename="onii_chan.mp3")
+#	await ctx.send(file=file)
 
-@client.command(aliases=["daisuki"])
-async def call_aski_daisuki(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You cannot use this command from this channel")
-	#v = random.choice(configs.Daisuki)
-	filename = os.path.join(os.getcwd() + "/Musics",choice(configs.Daisuki))
-	file = discord.File(filename,filename="daisuki.mp3")
-	await ctx.send(file=file)
+#@client.command(aliases=["daisuki"])
+#async def call_aski_daisuki(ctx):
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You cannot use this command from this channel")
+#	#v = random.choice(configs.Daisuki)
+#	filename = os.path.join(os.getcwd() + "/Musics",choice(configs.Daisuki))
+#	file = discord.File(filename,filename="daisuki.mp3")
+#	await ctx.send(file=file)
 
-@client.command(aliases=["onii-chan-song"])
-async def call_me_onii(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You cannot use this command from this channel")
-	if not isOnii_chan(ctx):
-		return await ctx.send("You are not my special onii-chan.  Baaakaaa...")
-	filename = os.path.join(os.getcwd() + "/Musics",configs.Onii_chan_song)
-	file = discord.File(filename,filename="onii-chan-song.mp3")
-	await ctx.send(file=file)
+#@client.command(aliases=["onii-chan-song"])
+#async def call_me_onii(ctx):
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You cannot use this command from this channel")
+#	if not isOnii_chan(ctx):
+#		return await ctx.send("You are not my special onii-chan.  Baaakaaa...")
+#	filename = os.path.join(os.getcwd() + "/Musics",configs.Onii_chan_song)
+#	file = discord.File(filename,filename="onii-chan-song.mp3")
+#	await ctx.send(file=file)
 
-@client.command(aliases=["special"])
-async def special_service(ctx):
-	if not isChannelNormal(ctx):
-		return await ctx.send("You cannot use this command from this channel")
-	if not isOnii_chan(ctx):
-		return await ctx.send("You are not my special onii-chan.  Baaakaaa...")
-	filename = os.path.join(os.getcwd() + "/Musics",configs.Onii_chan_special)
-	file = discord.File(filename,filename="special.mp3")
-	await ctx.send(file=file)
+#@client.command(aliases=["special"])
+#async def special_service(ctx):
+#	if not isChannelNormal(ctx):
+#		return await ctx.send("You cannot use this command from this channel")
+#	if not isOnii_chan(ctx):
+#		return await ctx.send("You are not my special onii-chan.  Baaakaaa...")
+#	filename = os.path.join(os.getcwd() + "/Musics",configs.Onii_chan_special)
+#	file = discord.File(filename,filename="special.mp3")
+#	await ctx.send(file=file)
 
 client.run(TOKEN)
